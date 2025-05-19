e1746cd3d14cd8f08629615e4491098cdc6cef80
PippinModels <ppandit@pippintitle.com>
2025-05-19 01:11:06 -0700

    Update streamlit_commercial_05_13.py

--- streamlit_commercial_05_13.py
+++ streamlit_commercial_05_13.py
@@ -48,7 +48,7 @@ if not df.empty:
         mapped_product = st.text_input("Enter your Mapped Product Ordered:")
     
     # Dropdown for 'Online/Offline' with 'Other' option
-    online_offline_options = list(df["Offline/Online"].unique()) 
+    online_offline_options = list(df["Offline/Online"].unique())
     online_offline = st.selectbox("Select Online/Offline", online_offline_options)
 
     # Filter Data based on user selections or "Other" inputs
