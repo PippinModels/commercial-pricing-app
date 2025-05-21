03dbca2f3c77f64c8a1d64d93d7823b1bb4ae9e7
PippinModels <ppandit@pippintitle.com>
2025-05-21 10:23:15 -0700

    Update streamlit_commercial_05_10.py

--- streamlit_commercial_05_10.py
+++ streamlit_commercial_05_10.py
@@ -95,6 +95,9 @@ if not df.empty:
             options=list(st.session_state.prediction_choices.keys()) + ["Other (Enter manually)"],
             index=None,
             label_visibility="collapsed"
+        )
+            index=None,
+            label_visibility="collapsed"
         )) + ["Other (Enter manually)"],
             index=None,
             label_visibility="collapsed"
