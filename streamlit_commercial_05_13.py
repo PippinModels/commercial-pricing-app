8086cb7a8cb66d80ae74857a4236219dec388770
Priyanka Pandit <priyanka.pandit0818@gmail.com>
2025-05-27 09:31:32 -0500

    other

--- streamlit_commercial_05_13.py
+++ streamlit_commercial_05_13.py
@@ -26,7 +26,7 @@ product_hierarchy = {
     "Full 60 YR Search": 7, "Full 80 YR Search": 8, "Full 100 YR Search": 9,
 }
 
-st.title("Commercial Prediction Model (05-23-2025)")
+st.title("Commercial Prediction Model (05-27-2025)")
 st.markdown("**Disclaimer:** Predicted pricing is based on a single parcel search.")
 
 if not df.empty:
