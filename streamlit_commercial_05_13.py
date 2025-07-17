6ef7a4d3189757866cf8fce72c91470c47b443f7
PippinModels <ppandit@pippintitle.com>
2025-07-17 14:30:51 -0500

    Update streamlit_commercial_05_13.py

--- streamlit_commercial_05_13.py
+++ streamlit_commercial_05_13.py
@@ -56,7 +56,7 @@ product_hierarchy = {
     "Full 60 YR Search": 7, "Full 80 YR Search": 8, "Full 100 YR Search": 9,
 }
 
-st.title("Commercial Prediction Model")
+st.title("Commercial Pricing Prediction Model")
 st.markdown("**Disclaimer:** Predicted pricing is based on a single parcel search.")
 
 if not df.empty:
